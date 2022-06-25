f8a44828ef6501eb174eab02bf4fd48545b62226
Ricsen <105400052+Ricsen12345@users.noreply.github.com>
2022-06-25 17:25:17 +0700

    Create UI_Automation.py

---
+++ UI_Automation.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class UI_Automation:
+  
+  def __init__(self, numQ:int, numA:list):
+    def.numQ = numQ
+    def.numA = numA
+    def.root = Tk()
+  
+  def createWidget():
+    pass
